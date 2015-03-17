e58594a1a6dd334a5f7ed501a8ba0288df0921f7
pzapata <pzapata@sigcomt.com>
2015-03-17 18:25:51 -0500

    se modifico el calendario

--- SistemaPacifico/Scripts/datepicker-es.js
+++ SistemaPacifico/Scripts/datepicker-es.js
@@ -27,6 +27,25 @@
         initStatus: 'Seleccionar fecha',
         isRTL: false
     };
+    
+    $.timepicker.regional['es'] = {
+        timeOnlyTitle: 'Elegir una hora',
+        timeText: 'Hora',
+        hourText: 'Horas',
+        minuteText: 'Minutos',
+        secondText: 'Segundos',
+        millisecText: 'Milisegundos',
+        microsecText: 'Microsegundos',
+        timezoneText: 'Uso horario',
+        currentText: 'Hoy',
+        closeText: 'Cerrar',
+        timeFormat: 'HH:mm',
+        timeSuffix: '',
+        amNames: ['a.m.', 'AM', 'A'],
+        pmNames: ['p.m.', 'PM', 'P'],
+        isRTL: false
+    };
 
     $.datepicker.setDefaults($.datepicker.regional['es']);
+    $.timepicker.setDefaults($.timepicker.regional['es']);
 });
